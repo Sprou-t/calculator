9b8bf919d4b6f96edd50fcd2306751e9f9a2c80e
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-04-05 17:42:16 +0800

    completed javascript operator function as well as html elements. Flexbox used to layout the structure

---
+++ javascript.js
@@ -0,0 +1,27 @@
+let x = Number(prompt(`what's x?`));
+let y = Number(prompt(`what's y?`));
+
+//add
+function add(x,y) {
+    return x+y;
+}
+//console.log(add(x,y)); //when u call function, must give arg
+
+//subtract
+function subtract(x,y){
+    return x-y;
+}
+//console.log(subtract(x,y))
+
+//multiply
+function multiply(x,y){
+    return x*y;
+}
+console.log(multiply(x,y))
+//divide
+function divide(x,y){
+    return x/y;
+}
+console.log(divide(x,y))
+//commit aft
+//for > advance operation use array and append the keys inside
