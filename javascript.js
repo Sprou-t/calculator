40811644be69a0da8ef09938dfe063ec03270e2b
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-04-08 00:31:09 +0800

    added the function that stores the numbers. However, need to change the function to exclude the other operators. Operator function should be in another function that utilises the operate() function depending on user selection and execute when user presses "="

--- javascript.js
+++ javascript.js
@@ -2,6 +2,27 @@ let firstNum;
 let operator;
 let secondNum;
 
+function display(){
+    //create an event function that stores the 3 variables using event delegation
+    let buttonSelect = document.querySelectorAll('.row');
+    let display = document.querySelector('.display'); //create a var to hold display val
+    let value; //create a variable to hold the number from the button pressed
+
+    buttonSelect.addEventListener('click',(event) => {
+        let target = event.target;
+
+        if (target.class==='number'){
+            value = target.innerHTML; //retrieves the html content of the button
+            display.value += value; //.value-->accessing the value property of input element. Each input has a value that is set by default or by user but usually it is left blank
+        }
+
+        else if (target.class==='divide' || target.class==='multiply' || target.class==='subtract'|| target.class==='add'|| target.class==='equal'|| target.class==='decimal'){
+            value = target.innerHTML; 
+            display.value += value;
+        }
+    })_
+}
+
 //create a function that calls upon whichever operator function
 function operate(firstNum,operator,secondNum){
     if (operator = '+'){
