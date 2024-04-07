fba070801a6e0fd53425959af942e432edff8025
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-04-07 23:45:04 +0800

    1. created a function: operate that delegates the math calculation wrt the operator being used. 2. Added colors to the buttons.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,45 @@
-let x = Number(prompt(`what's x?`));
-let y = Number(prompt(`what's y?`));
+let firstNum;
+let operator;
+let secondNum;
+
+//create a function that calls upon whichever operator function
+function operate(firstNum,operator,secondNum){
+    if (operator = '+'){
+        add(firstNum, secondNum);
+    }
+
+    else if (operator = '-'){
+        subtract(firstNum, secondNum);
+    }
+
+    else if (operator = 'x'){
+        multiply(firstNum, secondNum);
+    }
+
+    else {
+        divide(firstNum, secondNum);
+    }
+
+}
 
 //add
 function add(x,y) {
     return x+y;
 }
-//console.log(add(x,y)); //when u call function, must give arg
 
 //subtract
 function subtract(x,y){
     return x-y;
 }
-//console.log(subtract(x,y))
 
 //multiply
 function multiply(x,y){
     return x*y;
 }
-console.log(multiply(x,y))
+
 //divide
 function divide(x,y){
     return x/y;
 }
-console.log(divide(x,y))
-//commit aft
+
 //for > advance operation use array and append the keys inside
