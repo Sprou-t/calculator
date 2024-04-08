7f332f0355cdef90801e66f0fa191bc25b728243
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-04-08 19:22:17 +0800

    debugged the display function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-let firstNum;
-let operator;
-let secondNum;
+let firstNum = ''; 
+let operator = '';//assign an empty string to determine if no. is first or second
+let secondNum = '';
 
 function display(){
     //create an event function that stores the 3 variables using event delegation
@@ -8,19 +8,16 @@ function display(){
     let display = document.querySelector('.display'); //create a var to hold display val
     let value; //create a variable to hold the number from the button pressed
 
-    buttonSelect.addEventListener('click',(event) => {
-        let target = event.target;
+    buttonSelect.forEach(row => {
+        row.addEventListener('click',(event) => {
+            let target = event.target;
 
-        if (target.class==='number'){
-            value = target.innerHTML; //retrieves the html content of the button
-            display.value += value; //.value-->accessing the value property of input element. Each input has a value that is set by default or by user but usually it is left blank
-        }
-
-        else if (target.class==='divide' || target.class==='multiply' || target.class==='subtract'|| target.class==='add'|| target.class==='equal'|| target.class==='decimal'){
-            value = target.innerHTML; 
-            display.value += value;
-        }
-    })_
+            if (target.classList.contains('number')){
+                value = target.textContent; //retrieves the html content of the button
+                display.value += value; //.value-->accessing the value property of input element. Each input has a value that is set by default or by user but usually it is left blank
+            }
+        })
+    });
 }
 
 //create a function that calls upon whichever operator function
@@ -63,4 +60,5 @@ function divide(x,y){
     return x/y;
 }
 
+display();
 //for > advance operation use array and append the keys inside
